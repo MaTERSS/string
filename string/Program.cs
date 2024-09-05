02d09fbbe981e9f5faf0e6d02415b1881438b804
Ardor <Ardor@DESKTOP-BQF7240>
2024-09-05 13:38:34 +0800

    Добавьте файлы проекта.

---
+++ string/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Console.WriteLine("Назовите имя");
+string Name;
+Name = Console.ReadLine();
+
+Console.WriteLine("Назовите ваш возраст");
+int Age;
+Age = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("назовите ваш знак Зодиака");
+string Zodiac;
+Zodiac = Console.ReadLine();
+
+Console.WriteLine("Назовите место вашей работы");
+string Work;
+Work = Console.ReadLine();
+
+Console.WriteLine($"Ваше имя {Name}, вам {Age}, ваш знак зодиака {Zodiac},а Ваше место работы {Work}");
+
