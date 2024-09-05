248e130eefdae662ec18084c877df28caa5df8ce
MaTERSS <146178004+MaTERSS@users.noreply.github.com>
2024-09-05 16:02:21 +0800

    Update Program.cs

--- string/Program.cs
+++ string/Program.cs
@@ -6,19 +6,19 @@ Console.OutputEncoding = Encoding.Unicode;
 
 Console.WriteLine("Назовите имя");
 string name;
-Name = Console.ReadLine();
+name = Console.ReadLine();
 
 Console.WriteLine("Назовите ваш возраст");
 int age;
-Age = Convert.ToInt32(Console.ReadLine());
+age = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("назовите ваш знак Зодиака");
 string zodiac;
-Zodiac = Console.ReadLine();
+zodiac = Console.ReadLine();
 
 Console.WriteLine("Назовите место вашей работы");
 string work;
-Work = Console.ReadLine();
+work = Console.ReadLine();
 
 Console.WriteLine($"Ваше имя {name}, вам {age}, ваш знак зодиака {zodiac},а Ваше место работы {work}");
 
